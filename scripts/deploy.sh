18ab59654069ef4e1abeefe824e276dacb5158d0
George Cholerton <gcholerton@gmail.com>
2022-03-13 12:40:27 +0000

    Modified deploy script to keep node_modules

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,15 @@ set -euxo pipefail
 mkdir /tmp/mtgdeploy
 
 cp -r assets/* /tmp/mtgdeploy
-python3 scripts/process.py
+python3 scripts/process.py /tmp/mtgdeploy/cards.json
 npx esbuild src/index.js --bundle --minify --outfile=/tmp/mtgdeploy/index.js
 npx esbuild src/sw.js --bundle --minify --outfile=/tmp/mtgdeploy/sw.js
 
 git checkout gh-pages
-rm -r *
+shopt -s extglob
+rm -r -- !(node_modules)
 cp -r /tmp/mtgdeploy/* .
 rm -r /tmp/mtgdeploy
 git add -A
+git reset node_modules
 git commit -m "Deploy $(date +%F)"
