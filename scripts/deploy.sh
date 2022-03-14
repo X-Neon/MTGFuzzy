4d74917ef094e00f060662d849449e47bab66248
George Cholerton <gcholerton@gmail.com>
2022-03-14 19:29:55 +0000

    Modified deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,16 +2,17 @@
 
 set -euxo pipefail
 
+rm -rf .parcel-cache/ dist/
 mkdir /tmp/mtgdeploy
 
-cp -r assets/* /tmp/mtgdeploy
-python3 scripts/process.py /tmp/mtgdeploy/cards.json
-npx esbuild src/index.js --bundle --minify --outfile=/tmp/mtgdeploy/index.js
-npx esbuild src/sw.js --bundle --minify --outfile=/tmp/mtgdeploy/sw.js
+python3 scripts/process.py src/cards.dat
+npx parcel build --no-source-maps --dist-dir /tmp/mtgdeploy src/index.html
+rm src/cards.dat
 
 git checkout gh-pages
 shopt -s extglob
 rm -r -- !(node_modules)
+rm -r .parcel-cache/
 cp -r /tmp/mtgdeploy/* .
 rm -r /tmp/mtgdeploy
 git add -A
