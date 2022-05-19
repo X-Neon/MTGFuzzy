d66b4c5fef1b9282188461747f01dac40cff9aca
George Cholerton <gcholerton@gmail.com>
2022-05-19 22:08:10 +0100

    Modified valid card criteria

--- scripts/process.py
+++ scripts/process.py
@@ -14,12 +14,18 @@ def get_card_image_uris(card: dict) -> List[str]:
     raise RuntimeError("No card image")
 
 
+def is_legal(card: dict) -> bool:
+    if card["oversized"] or "legalities" not in card:
+        return False
+
+    return any((l == "legal" for l in card["legalities"].values()))
+
 def main() -> None:
     bulk_data = requests.get("https://api.scryfall.com/bulk-data/oracle-cards").json()
     card_uri = bulk_data["download_uri"]
     cards = requests.get(card_uri).json()
 
-    legal_cards = [c for c in cards if c["multiverse_ids"] and not c["oversized"]]
+    legal_cards = [c for c in cards if is_legal(c)]
     names = [{"name": c["name"], "uri": c["scryfall_uri"], "image": get_card_image_uris(c)} for c in legal_cards]
 
     with open(sys.argv[1], "w") as f:
